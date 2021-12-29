98d9f8b0411f7dea14e9d2ca54bbae5619634e93
cagri <cagri@DESKTOP-TP3O7NB>
2021-12-29 14:34:38 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç için Temel Kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya Başlangıç için Temel Kurs", "Java", "Python", "C++" };
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+        }
+    }
+}
