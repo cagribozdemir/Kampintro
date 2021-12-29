7f3d9165dbbfb04fe923dcaea1bc8abd82d8a6f2
cagribozdemir0 <cagribozdemir0@gmail.com>
2021-12-29 23:09:33 +0300

    4. Gün Dersi

---
+++ GenericsInro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class MyList<T>
+    {
+        T[] items;
+        //constructor - ctor
+        public MyList()
+        {
+            items=new T[0];
+        }
+        public void Add (T item)
+        {
+            T[] tempArray = items; 
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+ " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+    }
+}
